109af83ba16b9a1d265fa217feb0192460009cfe
choephix <i@thechoephix.com>
2016-03-19 00:02:53 +0200

    sdk update

--- prj desktop/bat/SetupSDK.bat
+++ prj desktop/bat/SetupSDK.bat
@@ -2,8 +2,8 @@
 
 :: Path to Flex SDK
 ::set FLEX_SDK=C:\flex_sdk
-set FLEX_SDK=D:\SDKs\AIRSDK
-
+::set FLEX_SDK=D:\SDKs\AIRSDK
+set FLEX_SDK=D:\Tools - SDKs\Flex 4.14.1 AIR 19
 
 :validation
 if not exist "%FLEX_SDK%" goto flexsdk
